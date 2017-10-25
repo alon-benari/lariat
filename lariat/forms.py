75757454ce912c94964a35309f32d78ecf86078f
alon-benari <alon.benari@gmail.com>
2017-10-25 08:36:06 -0700

    commit10252017

--- lariat/forms.py
+++ lariat/forms.py
@@ -5,8 +5,64 @@ class PatientForm(forms.ModelForm):
     """
     A form to capture data from a patient
     """
-    female = forms.BooleanField(label='female')
+    YES = 1
+    NO = 0
+    MALE = 0
+    FEMALE = 1
+    YES_NO= ((YES,'yes'),(NO,'no'))
+    GENDER = ((MALE,'male'),(FEMALE,'female'))
+    #
+    female = forms.ChoiceField(
+        choices = GENDER,
+        required=True, 
+        widget = forms.RadioSelect(),
+        label= 'Gender')
+    #
+    snf = forms.ChoiceField(
+        choices = YES_NO,
+        required=True, 
+        widget = forms.RadioSelect(),
+        label= 'Does the patient live in an assited living/nursing home environment?')
+    nephrologist = forms.ChoiceField(
+        choices = YES_NO,
+        required=True, 
+        widget = forms.RadioSelect(),
+        label = 'Has the patient ever seen a nephrologist or has a history of kidney disease?')
+    #
+    chf = forms.ChoiceField(
+        choices = YES_NO,
+        required=True, 
+        widget = forms.RadioSelect(),
+        label = 'Does the patient has chronic (long-standing) congestive heart failure?') 
+    #
+    sob = forms.ChoiceField(
+        choices = YES_NO,
+        required=True, 
+        widget = forms.RadioSelect(),
+        label = 'Does the patient CURRENTLY have shortness of breath at rest or mininal activity?')
+    #
+    cancer = forms.ChoiceField(
+        choices = YES_NO,
+        required=True, 
+        widget = forms.RadioSelect(),
+        label = 'Was the patient treated in the past 5 years for cancer?')
+    #
+    weight_loss = forms.ChoiceField(
+        choices = YES_NO,
+        required=True, 
+        widget = forms.RadioSelect(),
+        label = 'In the past 3 months, has the patient lost 10 pounds or more unintentionally?')
+    #
+    appetite = forms.ChoiceField(
+        choices = YES_NO,
+        required=True, 
+        widget = forms.RadioSelect(),
+        label = 'Is the patient appetite currently poor?')
+    
     class Meta:
-        model = Patient
+        model= Patient
         fields = ('female','first_name','last_name','age',
-                  'SSN','active_smoker','osa','climb2')
+                  'SSN',
+                  'snf','nephrologist','chf','sob','cancer',
+                  'weight_loss','appetite')
+    
--- lariat/models.py
+++ lariat/models.py
@@ -17,11 +17,18 @@ class Patient(models.Model):
     last_name = models.CharField(max_length=50)
     female = models.BooleanField(default=False)
     SSN = models.CharField(max_length=8)
-    active_smoker = models.BooleanField(default=False)
-    osa =  models.BooleanField(default=False)
-    climb2 =  models.BooleanField(default=False,help_text = 'can climb 2 flights of steps')
-    age = models.CharField(max_length=3,help_text = 'age')
-                
+    #active_smoker = models.BooleanField(default=False)
+    #osa =  models.BooleanField(default=False)
+    #climb2 =  models.BooleanField(default=False,help_text = 'can climb 2 flights of steps')
+    age = models.IntegerField(help_text = 'age')
+    snf = models.CharField(max_length=3,help_text = 'snf')
+    nephrologist = models.CharField(max_length=3)
+    chf =  models.CharField(max_length=3,help_text = 'CHF')
+    sob =  models.CharField(max_length=3,help_text = 'sob')
+    cancer =  models.CharField(max_length=3,help_text = 'cancer')
+    weight_loss = models.CharField(default = 'no',max_length=3,help_text = 'cancer')
+    appetite =  models.CharField(default = 'no',max_length=3,help_text = 'appetite')
+    
     def published(self):
         self.published_date = timezone.now()
         self.save()
